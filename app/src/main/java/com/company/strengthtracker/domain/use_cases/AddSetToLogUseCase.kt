30f9d3521e10396b025d883f67797bdcb4d9ec00
JosephGleespen <102929781+JosephGleespen@users.noreply.github.com>
2022-07-01 14:12:07 -0400

    Revert "Small changes to data organization, nothing substantial"

--- app/src/main/java/com/company/strengthtracker/domain/use_cases/AddSetToLogUseCase.kt
+++ app/src/main/java/com/company/strengthtracker/domain/use_cases/AddSetToLogUseCase.kt
@@ -3,21 +3,19 @@ package com.company.strengthtracker.domain.use_cases
 import android.content.ContentValues.TAG
 import android.util.Log
 import com.company.strengthtracker.data.entities.exercise_data.main_categories.AllExercises
-import com.company.strengthtracker.domain.repository.LogRepository
+import com.company.strengthtracker.data.repository.SetRepositoryImpl
+import com.company.strengthtracker.domain.repository.SetRepository
 import com.company.strengthtracker.domain.util.Resource
-import java.time.LocalDate
 import javax.inject.Inject
 
 class AddSetToLogUseCase @Inject constructor(
-    private val setRepositoryImpl: LogRepository
+    private val setRepositoryImpl: SetRepository
 ){
     suspend fun addSet(
         movement: AllExercises,
-        date: LocalDate,
-        userUid: String,
-        dateString: String = date.toString()
+        date: String,
+        userUid: String
     ):Resource<String> {
-        val dateIndex = hashMapOf("date" to date)
         val fields = hashMapOf(
             "name" to movement.name,
             "exType" to movement.exType.toString()
@@ -25,28 +23,22 @@ class AddSetToLogUseCase @Inject constructor(
 
         Log.d(TAG, "extype ----> ${movement.exType.toString()}")
 
-        return when(setRepositoryImpl.createLogPath(
-            fields,
-            dateIndex = dateIndex,
-            userUid,
-            dateString,
-            exType = movement.exType.toString(),
-            name = movement.name, )) {
-                is Resource.Success -> {
-                    when(setRepositoryImpl.addSet(movement, dateString, userUid)) {
+        val createPath = setRepositoryImpl.createLogPath(fields, userUid, date, exType = movement.exType.toString(), name = movement.name)
 
-                        is Resource.Success -> {
-                            Resource.Success("successful add")
-                        }
-                        is Resource.Error -> {
-                            Resource.Error("error: failed add")
-
-                        }
+        return when(createPath) {
+            is Resource.Success -> {
+                when(setRepositoryImpl.addSet(movement, date, userUid)) {
+                    is Resource.Success -> {
+                        Resource.Success("successful add")
+                    }
+                    is Resource.Error -> {
+                        Resource.Error("error: failed add")
                     }
                 }
-                is Resource.Error -> {
-                    Resource.Error("error: failed to create path")
-                }
+            }
+            is Resource.Error -> {
+                Resource.Error("error: failed to create path")
+            }
         }
     }
 }
